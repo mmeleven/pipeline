5d0cfcfa172e798eece56aa6b64294212f28b7a1
fyucz <fyu_cz@163.com>
2018-10-12 19:41:03 +0800

    bilstm_init

--- my_dl/my_bilstm/processing.py
+++ my_dl/my_bilstm/processing.py
@@ -3,6 +3,7 @@
 # import numpy as np
 import os
 
+# tag字典
 TAG = {
     '体育': 0,
     '女性': 1,
@@ -12,16 +13,25 @@ TAG = {
 
 
 def pre_processing(dir_path, out_path, mode='train'):
+    """
+    将文件夹的文件整理在一个txt中
+    :param dir_path:
+    :param out_path:
+    :param mode:
+    :return:
+    """
     tag_list = os.listdir(dir_path)
     fw = open(out_path, 'w', encoding='utf-8')
     for tag in tag_list:
-        tagpath = os.path.join(dir_path, tag)
-        txtlist = os.listdir(tagpath)
-        for txtname in txtlist:
-            filename = os.path.join(tagpath, txtname)
+        tag_path = os.path.join(dir_path, tag)
+        txt_list = os.listdir(tag_path)
+        for txt_name in txt_list:
+            filename = os.path.join(tag_path, txt_name)
             # print(filename)
             with open(filename, 'r', errors='ignore') as fr:
                 content = fr.readline()
+                if not content.split():
+                    continue
                 # print(content)
                 if mode == 'train':
                     fw.write(content + '\t' + str(TAG[tag]) + '\n')
@@ -30,7 +40,45 @@ def pre_processing(dir_path, out_path, mode='train'):
     fw.close()
 
 
+def cut_for_embedding(filename, out_path, stopword_path='data\\stop\\stopword.txt', mode='train'):
+    """
+    将原始预料去除停用词分词，用于词向量训练
+    :param filename:
+    :param out_path:
+    :param stopword_path:
+    :param mode:
+    :return:
+    """
+    import jieba
+    with open(stopword_path, 'r', encoding='utf-8') as fr:
+        stopwords = fr.readlines()
+    stopwords = [word.strip('\n') for word in stopwords]
+    with open(filename, 'r', encoding='utf-8') as fr:
+        lines = fr.readlines()
+    fw = open(out_path, 'w', encoding='utf-8')
+    for line in lines:
+        if mode == 'train':
+            if len(line.strip('\n').split('\t')) > 1:
+                content = line.strip('\n').split('\t')[0]
+                # print(content)
+                tag = line.strip('\n').split('\t')[1]
+        else:
+            content = line.strip('\n')
+        seg_list = list(jieba.cut(content))
+        for _ in seg_list[:]:
+            if _ in stopwords or len(_.strip()) == 0:
+                seg_list.remove(_)
+        if mode == 'train':
+            seg_list.append(tag)
+        fw.write('\t'.join(seg_list) + '\n')
+    fw.close()
+
+
 if __name__ == '__main__':
+    # 预处理
     pre_processing('data\\train_data', 'data\\clean\\train.txt')
     pre_processing('data\\test_data', 'data\\clean\\test_true.txt')
     pre_processing('data\\test_data', 'data\\clean\\test.txt', mode='test')
+    # 分词
+    cut_for_embedding('data\\clean\\train.txt', 'data\\clean\\train_seg.txt')
+    cut_for_embedding('data\\clean\\test.txt', 'data\\clean\\test_seg.txt', mode='test')
--- my_dl/my_bilstm/train.py
+++ my_dl/my_bilstm/train.py
@@ -2,6 +2,55 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
-def load_data()
+
+def load_embedding(model_path='data\\clean\\word2vec.model'):
+    """
+    加载词向量model
+    :param model_path:
+    :return:
+    """
+    import gensim
+    model = gensim.models.Word2Vec.load(model_path)
+    # print(model['我'])
+    return model
+
+
+def load_data(filename, model_path='data\\clean\\word2vec.model', sequence_length=20, mode='train'):
+    """
+    加载训练数据，返回np.array格式X,Y
+    :param filename:
+    :param model_path:
+    :param sequence_length:
+    :param mode:
+    :return:
+    """
+    embedding_model = load_embedding(model_path)
+    with open(filename, 'r', encoding='utf-8') as fr:
+        lines = fr.readlines()
+    X = np.zeros((len(lines), sequence_length, 100), dtype=np.float)
+    Y = np.zeros(len(lines))
+    for index, line in enumerate(lines):
+        split_list = line.strip('\r\n').split('\t')
+        if mode == 'train' and len(split_list) > 1:
+            seg_list = split_list[:-1]
+            if split_list[-1] in ['0', '1', '2', '3']:
+                Y[index] = float(split_list[-1])
+        else:
+            seg_list = split_list
+        for seg_index, seg in enumerate(seg_list):
+            if seg_index < sequence_length and seg in embedding_model:
+                X[index, seg_index, :] = embedding_model[seg]
+    if mode == 'train':
+        return X, Y
+    else:
+        return X
+
+
 if __name__ == '__main__':
-    pass
+    # load data
+    train_X, train_Y = load_data('data\\clean\\train_seg.txt')
+    print(np.shape(train_X))  # (3311, 20, 100)
+    print(np.shape(train_Y))  # (3311,)
+
+    test_X = load_data('data\\clean\\test_seg.txt', mode='test')
+    print(np.shape(test_X))  # (200, 20, 100)
